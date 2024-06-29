8138170854d771d215b30639a109300fe1e5ca23
ashaaa01 <152689190+ashaaa01@users.noreply.github.com>
2024-06-29 23:21:24 +0800

    Add files via upload

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Role;
+use Hash;
+use Spatie\Permission\Traits\HasRoles;
+use DB;
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+    
+    }
+}
